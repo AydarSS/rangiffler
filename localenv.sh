a8fa62deb9f1ffa684374f424eb30609beead3d5
AydarSS <www.sabitaidrl@mail.ru>
2024-05-05 16:31:51 +0300

    localenv.sh changed and settings

--- localenv.sh
+++ localenv.sh
@@ -4,6 +4,13 @@ echo '### Stop and remove containers ###'
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 
+
+echo "### Pull containers ###"
+# Пулл контейнеров
+docker pull mysql:8.0.33
+docker pull confluentinc/cp-zookeeper:7.3.2
+docker pull confluentinc/cp-kafka:7.3.2
+
 echo '### Docker run containers ###'
 docker run --name rangiffler-all -p 3306:3306 -e MYSQL_ROOT_PASSWORD=secret -v mysqldata:/var/lib/mysql -d mysql:8.0.33
 docker run --name=zookeeper -e ZOOKEEPER_CLIENT_PORT=2181 -e ZOOKEEPER_TICK_TIME=2000 -p 2181:2181 -d confluentinc/cp-zookeeper:7.3.2
@@ -14,6 +21,14 @@ docker run --name=kafka -e KAFKA_BROKER_ID=1 \
 -e KAFKA_TRANSACTION_STATE_LOG_MIN_ISR=1 \
 -e KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR=1 \
 -p 9092:9092 -d confluentinc/cp-kafka:7.3.2
+
+
+echo "### Create databases ###"
+sleep 30
+docker exec -i mysql_container mysql -uroot -psecret -e "CREATE DATABASE IF NOT EXISTS rangiffler_auth;"
+docker exec -i mysql_container mysql -uroot -psecret -e "CREATE DATABASE IF NOT EXISTS rangiffler_geo;"
+docker exec -i mysql_container mysql -uroot -psecret -e "CREATE DATABASE IF NOT EXISTS rangiffler_photo;"
+docker exec -i mysql_container mysql -uroot -psecret -e "CREATE DATABASE IF NOT EXISTS rangiffler_userdata;"
 
 
 echo '### Run frontend ###'
